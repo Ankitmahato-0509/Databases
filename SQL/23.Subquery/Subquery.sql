66faf328388aeecbd40829f07cbb2b9b155da023
Ankit Mahato <mahatoankit0007@gmail.com>
2026-03-21 23:53:51 +0530

    Updated Readme.md

---
+++ SQL/23.Subquery/Subquery.sql
@@ -0,0 +1,42 @@
+-- ==========================================
+-- CREATE TABLE: CUSTOMERS
+-- ==========================================
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name NVARCHAR(50),
+    city NVARCHAR(50)
+);
+
+-- ==========================================
+-- INSERT DATA INTO CUSTOMERS
+-- ==========================================
+INSERT INTO customers (customer_id, name, city)
+VALUES
+(1, 'Alice', 'Mumbai'),
+(2, 'Bob', 'Delhi'),
+(3, 'Charlie', 'Bangalore'),
+(4, 'David', 'Mumbai');
+
+
+-- ==========================================
+-- CREATE TABLE: ORDERS
+-- ==========================================
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT,
+    CONSTRAINT fk_customer 
+        FOREIGN KEY (customer_id) 
+        REFERENCES customers(customer_id)
+);
+
+-- ==========================================
+-- INSERT DATA INTO ORDERS
+-- ==========================================
+INSERT INTO orders (order_id, customer_id, amount)
+VALUES
+(101, 1, 500),
+(102, 1, 900),
+(103, 2, 300),
+(104, 4, 1700);
+
