f49d79e77b60054912976189d59d3746565b7173
Ankit Mahato <mahatoankit0007@gmail.com>
2026-03-29 08:28:54 +0530

    Update Left & Right Join.sql

--- SQL/19.Left & Right Join/Left & Right Join.sql
+++ SQL/19.Left & Right Join/Left & Right Join.sql
@@ -1,6 +1,6 @@
--- ==========================================
+
 -- CREATE TABLE: CUSTOMERS
--- ==========================================
+
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     name NVARCHAR(50),
@@ -8,9 +8,8 @@ CREATE TABLE customers (
 );
 
 
--- ==========================================
 -- INSERT DATA INTO CUSTOMERS
--- ==========================================
+
 INSERT INTO customers (customer_id, name, city)
 VALUES
 (1, 'Alice', 'Mumbai'),
@@ -19,9 +18,8 @@ VALUES
 (4, 'David', 'Mumbai');
 
 
--- ==========================================
 -- CREATE TABLE: ORDERS
--- ==========================================
+
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
@@ -29,9 +27,9 @@ CREATE TABLE orders (
     CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
--- ==========================================
+
 -- INSERT DATA INTO ORDERS
--- ==========================================
+
 INSERT INTO orders (order_id, customer_id, amount)
 VALUES
 (101, 1, 500),
@@ -39,20 +37,16 @@ VALUES
 (103, 2, 300),
 (104, 4, 1700);
 
-
-
--- ==========================================
 -- QUERY 1: Retrieve all customers and their orders (including customers without orders)
--- ==========================================
+
 SELECT *
 FROM customers c
 LEFT JOIN orders o
     ON c.customer_id = o.customer_id;
 
 
--- ==========================================
 -- QUERY 2: Retrieve all orders and their customers (including orders without customers)
--- ==========================================
+
 SELECT *
 FROM customers c
 RIGHT JOIN orders o
