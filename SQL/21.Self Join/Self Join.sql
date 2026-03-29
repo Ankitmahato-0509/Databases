a8b5e2c5014eba2762965313b1ed2468b9c2bc49
Ankit Mahato <mahatoankit0007@gmail.com>
2026-03-29 10:09:37 +0530

    Updated Readme.md

--- SQL/21.Self Join/Self Join.sql
+++ SQL/21.Self Join/Self Join.sql
@@ -1,15 +1,11 @@
--- ==========================================
 -- CREATE TABLE: EMPLOYEES
--- ==========================================
 CREATE TABLE employees (
     id INT PRIMARY KEY,
     name NVARCHAR(50),
     manager_id INT
 );
 
--- ==========================================
 -- INSERT DATA INTO EMPLOYEES
--- ==========================================
 INSERT INTO employees (id, name, manager_id)
 VALUES
 (1, 'Alice', NULL),
@@ -18,12 +14,8 @@ VALUES
 (4, 'David', 2),
 (5, 'Eva', 2);
 
--- ==========================================
 -- SELF JOIN: FIND EMPLOYEE AND THEIR MANAGER
--- ==========================================
 SELECT *
 FROM employees AS A
 JOIN employees AS B
 ON A.manager_id = B.id;
-
-
