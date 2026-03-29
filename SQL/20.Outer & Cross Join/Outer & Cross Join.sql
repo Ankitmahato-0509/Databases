a05c961accf95cf4f0d6053b6c957bfff9c23d53
Ankit Mahato <mahatoankit0007@gmail.com>
2026-03-29 10:10:29 +0530

    Updated readme.md

--- SQL/20.Outer & Cross Join/Outer & Cross Join.sql
+++ SQL/20.Outer & Cross Join/Outer & Cross Join.sql
@@ -1,15 +1,11 @@
--- ==========================================
 -- CREATE TABLE: CUSTOMERS
--- ==========================================
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     name NVARCHAR(50),
     city NVARCHAR(50)
 );
 
--- ==========================================
 -- INSERT DATA INTO CUSTOMERS
--- ==========================================
 INSERT INTO customers (customer_id, name, city)
 VALUES
 (1, 'Alice', 'Mumbai'),
@@ -17,10 +13,7 @@ VALUES
 (3, 'Charlie', 'Bangalore'),
 (4, 'David', 'Mumbai');
 
-
--- ==========================================
 -- CREATE TABLE: ORDERS
--- ==========================================
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
@@ -30,9 +23,7 @@ CREATE TABLE orders (
         REFERENCES customers(customer_id)
 );
 
--- ==========================================
 -- INSERT DATA INTO ORDERS
--- ==========================================
 INSERT INTO orders (order_id, customer_id, amount)
 VALUES
 (101, 1, 500),
@@ -40,10 +31,7 @@ VALUES
 (103, 2, 300),
 (104, 4, 1700);
 
-
--- ==========================================
 -- FULL OUTER JOIN USING LEFT + RIGHT JOIN
--- ==========================================
 SELECT *
 FROM customers AS c
 LEFT JOIN orders AS o
@@ -56,10 +44,7 @@ FROM customers AS c
 RIGHT JOIN orders AS o
 ON c.customer_id = o.customer_id;
 
-
--- ==========================================
 -- CROSS JOIN (CARTESIAN PRODUCT)
--- ==========================================
 SELECT *
 FROM customers
 CROSS JOIN orders;
