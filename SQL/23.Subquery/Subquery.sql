3430ad90dcb42f3d8586337308f8d62349b29808
Ankit Mahato <mahatoankit0007@gmail.com>
2026-03-29 10:07:29 +0530

    Update Readme.md

--- SQL/23.Subquery/Subquery.sql
+++ SQL/23.Subquery/Subquery.sql
@@ -2,27 +2,27 @@ CREATE DATABASE EXTRA;
 USE EXTRA;
 
 
--- ==========================================
+
 -- CREATE TABLE: customers
--- ==========================================
+
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     name NVARCHAR(50) NOT NULL,
     city NVARCHAR(50)
 );
 
--- ==========================================
+
 -- INSERT DATA INTO customers
--- ==========================================
+
 INSERT INTO customers (customer_id, name, city) VALUES
 (1, 'Alice', 'Mumbai'),
 (2, 'Bob', 'Delhi'),
 (3, 'Charlie', 'Bangalore'),
 (4, 'David', 'Mumbai');
 
--- ==========================================
+
 -- CREATE TABLE: orders
--- ==========================================
+
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_id INT NOT NULL,
@@ -32,18 +32,17 @@ CREATE TABLE orders (
         REFERENCES customers(customer_id)
 );
 
--- ==========================================
 -- INSERT DATA INTO orders
--- ==========================================
+
 INSERT INTO orders (order_id, customer_id, amount) VALUES
 (101, 1, 500),
 (102, 1, 900),
 (103, 2, 300),
 (104, 4, 1700);
 
--- ==========================================
+
 -- QUERY: Orders with amount greater than average
--- ==========================================
+
 SELECT o.order_id,
        o.customer_id,
        o.amount
